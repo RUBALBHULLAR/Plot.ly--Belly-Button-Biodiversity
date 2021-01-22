9d96b4f29bae059366b2e6bf9c3752741077163b
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2021-01-22 02:46:41 -0700

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -107,7 +107,17 @@ function optionChanged(id) {
 function init(){
       // select dropdown menu 
       var dropdown = d3.select("#selDataset");
-      
+    
+    // read the data 
+    d3.json("samples.json").then((data)=> {
+        console.log(data)
+
+        // get the id data to the dropdwown menu
+        data.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+
+
 }
 
 
