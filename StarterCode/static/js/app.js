8f94521ff519de85f12af6a50beb2e2680de95ee
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2021-01-23 01:56:17 -0700

    changes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,7 +2,7 @@ function getPlots(id){
    //Read samples.json
    d3.json("samples.json").then(sampledata =>
     {
-        console.log(samledata)
+        console.log(sampledata)
         var ids = sampledata.samples[0].otu_ids;
         console.log(ids)
         var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
